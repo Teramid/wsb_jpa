c2cb04faa6f3540a9b7bd81cb0086a393e229697
Betwanxd <konrad.okon@vp.pl>
2025-01-06 20:00:21 +0100

    Dao

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/PatientDaoImpl.java
@@ -3,8 +3,12 @@ package com.jpacourse.persistence.dao.impl;
 import com.jpacourse.persistence.dao.PatientDao;
 import com.jpacourse.persistence.entity.PatientEntity;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import com.jpacourse.service.PatientService;
 
 @Repository
 public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
-
+    @Override
+    public void addVisitToPatient(Long patientId, Long doctorId, LocalDateTime visitDate, String description) {
+        PatientEntity patient = PatientService.findPatientById(patientId);
 }
